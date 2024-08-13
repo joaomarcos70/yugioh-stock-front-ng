433fae7a55caedd43c75c5204ac72b2d3e2b4d4c
Joao Albuquerque <joaomarcos70@outlook.com.br>
2024-08-13 17:06:12 -0300

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,58 +1,58 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser'
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core'
+import { HttpClientModule } from '@angular/common/http'
+import { RouterModule } from '@angular/router'
 
-import { AppComponent } from '../app/views/app.component';
-import { AppRoutes } from './app.routing';
-import { ConfigService } from './config/config.service';
-import { LoginService } from './services/login.service';
-import { LoginComponent } from './views/login/login.component';
-import { FormsModule } from '@angular/forms';
-import { NgZorroAntdModule, NZ_I18N, pt_BR } from 'ng-zorro-antd';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
-import pt from '@angular/common/locales/pt';
-import { HomeComponent } from './views/home/home.component';
-import { BaseComponent } from './shared/components/base/base.component';
-import { SearchCardComponent } from './views/search-card/search-card.component';
-import { YGOservice } from './services/YGO.service';
-import { NgxPaginationModule } from 'ngx-pagination';
-import { AddCollectionComponent } from './views/add-collection/add-collection.component';
-import { SpinnerComponent } from './shared/components/spinner/spinner.component';
-import { UiSwitchModule } from 'ngx-toggle-switch';
-import { FilterComponent } from './shared/components/filter/filter.component';
+import { AppComponent } from '../app/views/app.component'
+import { AppRoutes } from './app.routing'
+import { ConfigService } from './config/config.service'
+import { LoginService } from './services/login.service'
+import { LoginComponent } from './views/login/login.component'
+import { FormsModule } from '@angular/forms'
+import { NzLayoutModule } from 'ng-zorro-antd/layout'
+import { NzMenuModule } from 'ng-zorro-antd/menu'
+import { NzIconModule } from 'ng-zorro-antd/icon'
+import { NZ_I18N } from 'ng-zorro-antd/i18n'
+import { pt_BR } from 'ng-zorro-antd/i18n'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { registerLocaleData } from '@angular/common'
+import pt from '@angular/common/locales/pt'
+import { HomeComponent } from './views/home/home.component'
+import { BaseComponent } from './shared/components/base/base.component'
+import { SearchCardComponent } from './views/search-card/search-card.component'
+import { YGOservice } from './services/YGO.service'
+//import { NgxPaginationModule } from 'ngx-pagination'
+import { AddCollectionComponent } from './views/add-collection/add-collection.component'
+import { SpinnerComponent } from './shared/components/spinner/spinner.component'
+//import { UiSwitchModule } from 'ngx-toggle-switch';
+import { FilterComponent } from './shared/components/filter/filter.component'
 
-
-registerLocaleData(pt);
+registerLocaleData(pt)
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent,
-    BaseComponent,
-    SearchCardComponent,
-    AddCollectionComponent,
-    SpinnerComponent,
-    FilterComponent,
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    RouterModule.forRoot(AppRoutes),
-    FormsModule,
-    NgZorroAntdModule,
-    BrowserAnimationsModule,
-    NgxPaginationModule,
-    UiSwitchModule
-  ],
-  providers: [
-    ConfigService,
-    LoginService,
-    YGOservice,
-    { provide: NZ_I18N, useValue: pt_BR }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        HomeComponent,
+        BaseComponent,
+        SearchCardComponent,
+        AddCollectionComponent,
+        SpinnerComponent,
+        FilterComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        RouterModule.forRoot(AppRoutes),
+        FormsModule,
+        NzLayoutModule,
+        NzMenuModule,
+        NzIconModule,
+        BrowserAnimationsModule
+        //NgxPaginationModule
+    ],
+    providers: [ConfigService, LoginService, YGOservice, { provide: NZ_I18N, useValue: pt_BR }],
+    bootstrap: [AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {}
