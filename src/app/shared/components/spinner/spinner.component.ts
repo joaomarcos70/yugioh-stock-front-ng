d9819d6d69757c3e9184686d126e9762d3619858
Joao Albuquerque <joaomarcos70@outlook.com.br>
2024-08-13 21:05:01 -0300

    fix:home-layout

--- src/app/shared/components/spinner/spinner.component.ts
+++ src/app/shared/components/spinner/spinner.component.ts
@@ -1,48 +1,48 @@
-import { Component, Inject, Input, OnDestroy, ViewEncapsulation } from '@angular/core';
-import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
-import { DOCUMENT } from '@angular/common';
-import { Spinkit } from './spinkits';
+import { Component, Inject, Input, OnDestroy, ViewEncapsulation } from '@angular/core'
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router'
+import { DOCUMENT } from '@angular/common'
+import { Spinkit } from './spinkits'
 
 @Component({
-  selector: 'app-spinner',
-  templateUrl: './spinner.component.html',
-  styleUrls: [
-    './spinner.component.css',
-    './spinkit-css/sk-double-bounce.css',
-    './spinkit-css/sk-chasing-dots.css',
-    './spinkit-css/sk-cube-grid.css',
-    './spinkit-css/sk-rotating-plane.css',
-    './spinkit-css/sk-spinner-pulse.css',
-    './spinkit-css/sk-three-bounce.css',
-    './spinkit-css/sk-wandering-cubes.css',
-    './spinkit-css/sk-wave.css',
-    './spinkit-css/sk-line-material.css'
-  ]
+    selector: 'app-spinner',
+    templateUrl: './spinner.component.html',
+    styleUrls: [
+        './spinner.component.css',
+        './spinkit-css/sk-double-bounce.css',
+        './spinkit-css/sk-chasing-dots.css',
+        './spinkit-css/sk-cube-grid.css',
+        './spinkit-css/sk-rotating-plane.css',
+        './spinkit-css/sk-spinner-pulse.css',
+        './spinkit-css/sk-three-bounce.css',
+        './spinkit-css/sk-wandering-cubes.css',
+        './spinkit-css/sk-wave.css',
+        './spinkit-css/sk-line-material.css'
+    ]
 })
 export class SpinnerComponent implements OnDestroy {
+    public isSpinnerVisible = true
+    public Spinkit = Spinkit
 
-  public isSpinnerVisible = true;
-  public Spinkit = Spinkit;
+    @Input() public backgroundColor = 'rgba(0, 115, 170, 0.69)'
+    @Input() public spinner = Spinkit.skLine
 
-  @Input() public backgroundColor = 'rgba(0, 115, 170, 0.69)';
-  @Input() public spinner = Spinkit.skLine;
+    constructor(private router: Router, @Inject(DOCUMENT) private document: Document) {
+        this.router.events.subscribe({
+            next: event => {
+                if (event instanceof NavigationStart) {
+                    this.isSpinnerVisible = true
+                } else if (
+                    event instanceof NavigationEnd ||
+                    event instanceof NavigationCancel ||
+                    event instanceof NavigationError
+                ) {
+                    this.isSpinnerVisible = false
+                }
+            }
+        })
+    }
 
-  constructor(private router: Router, @Inject(DOCUMENT) private document: Document) {
-    console.log("carreguei");
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationStart) {
-        this.isSpinnerVisible = true;
-      } else if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
-        this.isSpinnerVisible = false;
-      }
-    }, () => {
-      this.isSpinnerVisible = false;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.isSpinnerVisible = false;
-  }
-
-
+    ngOnDestroy(): void {
+        this.isSpinnerVisible = false
+    }
 }
--- src/app/views/search-card/search-card.component.ts
+++ src/app/views/search-card/search-card.component.ts
@@ -1,74 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 import _ from 'lodash'
-import { cardSearch } from 'src/app/models/CardSearch.model';
-import { YGOservice } from 'src/app/services/YGO.service';
-import { CardInterface } from 'src/app/interfaces/cardSearch.interface';
-import { Router } from '@angular/router';
-import { Location } from '@angular/common';
+import { cardSearch } from 'src/app/models/CardSearch.model'
+import { YGOservice } from 'src/app/services/YGO.service'
+import { CardInterface } from 'src/app/interfaces/cardSearch.interface'
+import { Router } from '@angular/router'
+import { Location } from '@angular/common'
 
 @Component({
-  selector: 'app-search-card',
-  templateUrl: './search-card.component.html',
-  styleUrls: ['./search-card.component.css']
+    selector: 'app-search-card',
+    templateUrl: './search-card.component.html',
+    styleUrls: ['./search-card.component.css']
 })
 export class SearchCardComponent implements OnInit {
+    canView: boolean = false
+    params: CardInterface = { fname: 'witch' }
+    nameOfCard: string
+    foundCards: number = 0
+    txtFoundCards: string = ''
 
-  canView: boolean = false
-  params: CardInterface = { fname: 'fluffal' };
-  nameOfCard: string
-  foundCards: number = 0
-  txtFoundCards: string ='';
+    itemsPerPage: number[] = [10, 50, 100]
+    totalItems: number = 0
+    currentPage: number = 1
 
-  itemsPerPage: number[] = [10, 50, 100];
-  totalItems: number = 0;
-  currentPage: number = 1;
+    cards: CardInterface[] = new Array<CardInterface>()
+    cardSearchModel: cardSearch = new cardSearch()
 
-  cards: CardInterface[] = new Array<CardInterface>()
-  cardSearchModel: cardSearch = new cardSearch()
+    showSideMenu: boolean = false
 
-  showSideMenu:boolean = false
+    constructor(private YgoService: YGOservice, private router: Router, private location: Location) {}
 
+    async ngOnInit() {
+        this.searchCards()
+    }
 
+    previousPage() {
+        this.location.back()
+    }
 
-  constructor(private YgoService: YGOservice,
-    private router:Router,
-    private location: Location) { }
+    filterAction() {
+        this.showSideMenu = !this.showSideMenu
+    }
 
-  async ngOnInit() {
-    this.searchCards()
-  }
+    searchCards() {
+        this.YgoService.get(this.params).subscribe(res => {
+            this.cards = res.data
+            this.foundCards = res.data.length
 
-  previousPage(){
-    this.location.back()
-  }
+            if (this.foundCards == 0) {
+                this.txtFoundCards = 'Nenhuma carta encontrada'
+            } else if (this.foundCards > 1) {
+                this.txtFoundCards = this.foundCards + ' cartas encontradas'
+            } else {
+                this.txtFoundCards = this.foundCards + ' carta encontrada'
+            }
+        })
+    }
 
-  filterAction(){
-    this.showSideMenu = !this.showSideMenu
-  }
+    addCollection(cardId: number) {
+        this.router.navigate([`/add-collection/${cardId}`])
+    }
 
-  searchCards() {
-
-      this.YgoService.get(this.params).subscribe(res => {
-        this.cards = res.data
-        
-        this.foundCards = res.data.length
-
-        if(this.foundCards == 0 ){
-          this.txtFoundCards ='Nenhuma carta encontrada'
-        }else if(this.foundCards > 1){
-          this.txtFoundCards = this.foundCards + ' cartas encontradas'
-        } else {
-          this.txtFoundCards = this.foundCards + ' carta encontrada'
-        }
-      })
-
-  }
-
-  addCollection(cardId:number){
-    this.router.navigate([`/add-collection/${cardId}`])
-  }
-
-  show() {
-    this.canView = !this.canView
-  }
+    show() {
+        this.canView = !this.canView
+    }
 }
