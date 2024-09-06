8cb675f754c766cc3e9089587f84f21385cf553c
Joao Albuquerque <joaomarcos70@outlook.com.br>
2024-09-05 22:56:12 -0300

    feat(ygowants): layout add card

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,24 +1,25 @@
-import { Routes } from '@angular/router';
-import { AddCollectionComponent } from './views/add-collection/add-collection.component';
-import { HomeComponent } from './views/home/home.component';
-import { LoginComponent } from './views/login/login.component';
-import { SearchCardComponent } from './views/search-card/search-card.component';
+import { Routes } from '@angular/router'
+import { AddCollectionComponent } from './views/add-collection/add-collection.component'
+import { HomeComponent } from './views/home/home.component'
+import { LoginComponent } from './views/login/login.component'
+import { SearchCardComponent } from './views/search-card/search-card.component'
 
 export const AppRoutes: Routes = [
     {
-        path:"",
-        component:LoginComponent
+        path: '',
+        component: LoginComponent
     },
     {
-        path:"home",
-        component:HomeComponent
+        path: 'home',
+        component: HomeComponent
     },
     {
-        path:"search-card",
-        component:SearchCardComponent
+        path: 'search-card',
+        component: SearchCardComponent
     },
     {
-        path:"add-collection/:id",
-        component:AddCollectionComponent
-    }
+        path: 'add-collection/:id',
+        component: AddCollectionComponent
+    },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ]
